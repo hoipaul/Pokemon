71b0d63c6b6577acfcab1ba14a8e658caed96fa2
Paul Hermans <paulhermans@MacBook-Air-van-Paul.local>
2021-06-15 11:13:41 +0200

    huiswerk commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import './App.css';
+import axios from "axios";
+import React, {useEffect, useState} from "react";
+import "./App.css";
+import PokemonCard from "./PokemonCard";
 
 function App() {
-  return (
-    <div>
-      Begin hier met de opdracht!
-    </div>
-  );
+    const [pokenames, setPokenames] = useState(null);
+
+    useEffect(() => {
+        console.log("fetch names here");
+
+        async function fetchNames() {
+            const response = await axios.get(
+                "https://pokeapi.co/api/v2/pokemon?limit=20"
+            );
+            console.log(response.data.results);
+            setPokenames(response.data.results);
+        }
+            fetchNames();
+        }, []);
+
+    console.log(pokenames);
+
+    return (
+        <div>
+            {pokenames ? (
+                <div>
+                    <button>Vorige</button>
+                    <button>Volgende</button>
+                    {pokenames.map((pokemon) => {
+                        return <PokemonCard nameOfPokemon={pokemon.name}/>;
+                    })}
+                </div>
+            ) : (
+                <h3>Loading</h3>
+            )}
+        </div>
+    );
 }
 
 export default App;
